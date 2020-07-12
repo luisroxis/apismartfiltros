8c64f322d3f16719c88f4475a3666d575d36f75d
Luis Carlos <roxisxprol@gmail.com>
2020-07-12 17:52:29 -0300

    ssl test

--- src/server.js
+++ src/server.js
@@ -1,3 +1,18 @@
 import app from './app'
+import https from 'https'
+import http from 'http'
+import fs from 'fs'
 
-app.listen(9002)
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer({
+  key: fs.readFileSync('./src/certs/privatekey.rtf'),
+  cert: fs.readFileSync('./src/certs/csr.rtf'),
+}, app);
+
+httpServer.listen(9001, () => {
+    console.log('HTTP Server running on port 9001');
+});
+
+httpsServer.listen(9002, () => {
+    console.log('HTTPS Server running on port 9002');
+});
