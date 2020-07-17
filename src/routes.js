27965959a025f756706e4b5b66a548a80b769004
Luis Carlos <roxisxprol@gmail.com>
2020-07-17 17:30:27 -0300

    security api

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 import  { Router } from  'express'
+import Brute from 'express-brute'
+import BruteRedis from 'express-brute-redis'
 import  UserController from './app/controllers/UserController'
 import  SessionController from './app/controllers/SessionController'
 import  ClientController from './app/controllers/ClientController'
@@ -6,14 +8,19 @@ import  ClientController from './app/controllers/ClientController'
 import  authMiddleware from  './app/middleware/auth'
 
 const routes = new Router()
+const bruteStore = new BruteRedis({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT
+})
 
+const bruteForce = new Brute(bruteStore)
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Bem vindo a SmartFiltros' })
 })
 
 routes.post('/users', UserController.store)
-routes.post('/sessions', SessionController.store)
+routes.post('/sessions',bruteForce.prevent, SessionController.store)
 routes.post('/clients', ClientController.store)
 
 routes.use(authMiddleware)
